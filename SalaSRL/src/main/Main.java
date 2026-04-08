44e4c3cc28365d88e0ea452751ded828d7338c32
NicoloCarlini <nicolocarlini@teamdigitale.torricellimi.edu.it>
2026-04-09 00:57:41 +0200

    prossime correzioni

--- SalaSRL/src/main/Main.java
+++ SalaSRL/src/main/Main.java
@@ -5,5 +5,8 @@ import javax.swing.*;
 public class Main {
 	public static void main(String[] nmf) throws IOException {
 		new Collegamenti();
+		
+		//sistemare grandezze e calcoli scrollbar
+		//mettere che se elimini l'intero elemento da dal carrello viene eliminato dal carrello
 	}
 }
