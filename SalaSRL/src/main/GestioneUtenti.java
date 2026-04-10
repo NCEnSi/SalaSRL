e8e9758bcbc825c9a25f210b73c2d172aba1d0fa
manue <manue@Manuel>
2026-04-10 15:30:56 +0200

    inizo schermata elenco utenti

---
+++ SalaSRL/src/main/GestioneUtenti.java
@@ -0,0 +1,15 @@
+package main;
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class GestioneUtenti extends JPanel{
+
+	//VARIABILI DI ISTANZA
+	//bottone per tornare alla schermata precedente
+	
+	
+	
+	
+	
+}
